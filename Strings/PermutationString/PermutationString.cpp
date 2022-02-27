ed871bc9bb111970869bedd6a04c7d6beed8de25
Hitesh Gorantla <74699575+GSHitesh@users.noreply.github.com>
2022-02-27 07:42:25 +0530

    Permutation of a String

--- Strings/PermutationString/PermutationString.cpp
+++ Strings/PermutationString/PermutationString.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
+void perString(string S, int l,int h){
+
+    if(l == h)
+        cout << S << " ";
+    else{
+        for(int i =l;i<=h;i++){
+            swap(S[l],S[i]);
+            perString(S,l+1,h);
+            swap(S[l],S[i]);
+        }
+    }
+}
+
 int main(){
 
-    cout << "First Program in VS";
+    // Works for only distinct letter in a String
+    string s = "abc";
+    perString(s,0,2);
     return 0;
 }
